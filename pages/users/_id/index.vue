1c885ee52476a0bff3c9897e4ca8581d2a533ff7
Miyuki Harada <mykhrd@gmail.com>
2018-03-15 15:19:51 +0900

    add validation

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -7,3 +7,16 @@ dynamic path
         <p>user ID: {{ $route.params.id }}</p>
     </div>
 </template>
+<script>
+export default {
+  // validate
+  validate(data) {
+    console.log(data)
+    // validate value
+    // return data.params.id == 1
+
+    // regex
+    return /^\d+$/.test(data.params.id)
+  }
+}
+</script>
