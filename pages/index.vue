6675ea6814547e17d2d9dd3c5c5958b4bb1b9606
Miyuki Harada <mykhrd@gmail.com>
2018-04-13 15:12:59 +0900

    update code

--- pages/index.vue
+++ pages/index.vue
@@ -5,24 +5,9 @@
       <p>intro</p>
     </section>
     <section>
-      <PostPreview 
-        id="1"
-        thumbnail="//images.pexels.com/photos/34600/pexels-photo.jpg"
-        title="hello there!"
-        previewText="First post!"
-        />
-      <PostPreview 
-        id="1"
-        thumbnail="//images.pexels.com/photos/270366/pexels-photo-270366.jpeg"
-        title="hello there!"
-        previewText="second post!"
-        />
-      <PostPreview 
-        id="1"
-        thumbnail="//images.pexels.com/photos/34600/pexels-photo.jpg"
-        title="hello there!"
-        previewText="third post!"
-        />
+      <PostPreview id="1" thumbnail="//images.pexels.com/photos/34600/pexels-photo.jpg" title="hello there!" previewText="First post!" />
+      <PostPreview id="1" thumbnail="//images.pexels.com/photos/270366/pexels-photo-270366.jpeg" title="hello there!" previewText="second post!" />
+      <PostPreview id="1" thumbnail="//images.pexels.com/photos/34600/pexels-photo.jpg" title="hello there!" previewText="third post!" />
     </section>
   </div>
 </template>
